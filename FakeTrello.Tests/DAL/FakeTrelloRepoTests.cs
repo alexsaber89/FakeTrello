d8a84ee183aac3bc8c76cc036d7db6b85b9eb840
Alex Saber <alexsaber89@gmail.com>
2017-03-13 18:56:12 -0500

    add initial IsNotNull tests

---
+++ FakeTrello.Tests/DAL/FakeTrelloRepoTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeTrello.DAL;
+
+namespace FakeTrello.Tests.DAL
+{
+    [TestClass]
+    public class FakeTrelloRepoTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateAnInstanceOfRepo()
+        {
+            FakeTrelloRepository Repo = new FakeTrelloRepository();
+
+            Assert.IsNotNull(Repo);
+        }
+
+        [TestMethod]
+        public void EnsureIHaveNotNullContext()
+        {
+            FakeTrelloRepository Repo = new FakeTrelloRepository();
+
+            Assert.IsNotNull(Repo.Context);
+        }
+
+        [TestMethod]
+        public void EnsureICanInjectContextInstance()
+        {
+            FakeTrelloContext context = new FakeTrelloContext();
+            FakeTrelloRepository repo = new FakeTrelloRepository(context);
+
+            Assert.IsNotNull(repo.Context);
+        }
+    }
+}
